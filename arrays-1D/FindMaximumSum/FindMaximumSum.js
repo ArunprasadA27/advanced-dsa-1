2c171a190c305a2859143b6d262c9b60a2386f5e
Arunprasad A <168812123+ArunprasadA27@users.noreply.github.com>
2025-06-18 19:48:20 +0000

    Brute Force 2

--- arrays-1D/FindMaximumSum/FindMaximumSum.js
+++ arrays-1D/FindMaximumSum/FindMaximumSum.js
@@ -1,9 +1,11 @@
-/* 
+/* 1. Brute Force 1
+--------------------
     I/P => A = [1, 2, -1, 3]
     O/P => 5
-
+    T.C     => O(N^3) - since 3 loops are running 
+    S.C     => O(1)
 */
-function FindMaximumSum_BF(A) {
+function FindMaximumSum_BF1(A) {
     let maxSum = Number.NEGATIVE_INFINITY;
     for(let i=0; i<A.length; i++){
         for(let j=i; j<A.length; j++){
@@ -17,4 +19,32 @@ function FindMaximumSum_BF(A) {
     return maxSum;
 }
 
-console.log(FindMaximumSum_BF([1, 2, -1, 3]));
+// console.log('Brute Force - 1 ', FindMaximumSum_BF1([1, 2, -1, 3]));
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* 2. Brute Force 2
+--------------------
+    I/P     => A = [1, 2, -1, 3]
+    O/P     => 5
+    T.C     => O(N^2)
+    S.C     => O(1)
+*/
+function FindMaximumSum_BF2(A) {
+    let maxSum = Number.NEGATIVE_INFINITY;
+    for(let i=0; i<A.length; i++){
+        let currSum = 0;
+        for(let j=i; j<A.length; j++){
+            currSum += A[j];
+            maxSum = Math.max(maxSum, currSum);
+        }
+    }
+    return maxSum;
+}
+
+// console.log('Brute Force - 2 ', FindMaximumSum_BF2([1, 2, -1, 3]));
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+/* 3. Brute Force 2
+--------------------
+    I/P => A = [1, 2, -1, 3]
+    O/P => 5
+*/
