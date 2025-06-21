e53867b5e75704cafb6f1f4943c2ac57fe8adf90
Arunprasad A <168812123+ArunprasadA27@users.noreply.github.com>
2025-06-21 13:35:37 +0000

    Begger problem Optimized solution.

--- arrays-1D/Q2.ContinuousSumQuery/Solution.js
+++ arrays-1D/Q2.ContinuousSumQuery/Solution.js
@@ -26,17 +26,24 @@ function BruteForce1(A, B){
 
 function ContinuousSumQuery(A, B){
     let result = new Array(A).fill(0);
+    let newResult = new Array(A).fill(0);
 
     for(let i=0; i<B.length; i++){
         let l = B[i][0];
         let r = B[i][1];
-
-        if(r < A){
-            result[]
-        }
+        
         result[l-1] = result[l-1] + B[i][2];
+        if(r < A){
+            result[r] = result[r] - B[i][2];
+        } 
     }
-    return result;
+
+    newResult[0] = result[0];
+    for(let i=1; i<result.length; i++){
+        newResult[i] = newResult[i-1] + result[i]; 
+    }
+
+    return newResult;
 }
 console.log('Optimized Solution - ', ContinuousSumQuery(5, [[1, 2, 10], [2, 3, 20], [2, 5, 25]]));
 // ----------------------------------2. Optimized solution----------------------------------------------------------------------------------------------------------------
